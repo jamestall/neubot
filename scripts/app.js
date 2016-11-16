92d4aaa8736699f28fb49db837dfacd6cc8df51c
James Hall <james.hall@mulesoft.com>
2016-11-15 20:44:32 -0800

    squirrels

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,13 @@ module.exports = function(bot) {
 }
 
 var squirrels = [
-  "http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg",
-  "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png",
   "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png",
-  "http://shipitsquirrel.github.com/images/squirrel.png"
+  "http://shipitsquirrel.github.com/images/squirrel.png",
+  "https://media.giphy.com/media/143vPc6b08locw/giphy.gif",
+  "http://i.imgur.com/DPVM1.png",
+  "http://s2.quickmeme.com/img/e0/e07b5437703f88eb4229adc1c6931061d4c5557f761c048dac3d12f9b8e4846a.jpg",
+  "http://s.quickmeme.com/img/9a/9a0066a21027716e7b0b5b900f3c2af295fdcad706ea6836fcb58efdeb261c8e.jpg",
+  "http://i.memecaptain.com/gend_images/zyCwXg.jpg"
 ]
 
 module.exports = function(bot) {
