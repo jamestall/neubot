e06905b5d1ec75c3c7f22a1a49a68636b809232d
James Hall <james.hall@mulesoft.com>
2016-11-22 17:01:11 -0800

    added 9gag

--- scripts/app.js
+++ scripts/app.js
@@ -145,6 +145,14 @@ var helpMe = [
 "booleans		=		Booleans"
 ]
 
+//setup for 9gag
+var HTMLParser, Select, escape_html_characters, get_meme_image, get_meme_title, select_element, send_meme;
+
+Select = require("soupselect").select;
+
+HTMLParser = require("htmlparser");
+
+
 module.exports = function(bot) {
 	//That's cold
 	 bot.hear(/That's Cold/, function(res) {
@@ -263,6 +271,78 @@ module.exports = function(bot) {
 		 } 
 		return res.send("Say this...			and I'll tell you about...");
 	});
+
+	//9gag code
+	return robot.respond(/9gag( me)?/i, function(message) {
+    return send_meme(message, false, function(title, src) {
+      return message.send(title, src);
+    });
+  });
+};
+
+send_meme = function(message, location, response_handler) {
+  var meme_domain, url;
+  meme_domain = "http://9gag.com";
+  location || (location = "/random");
+  if (location.substr(0, 4) !== "http") {
+    url = meme_domain + location;
+  } else {
+    url = location;
+  }
+  return message.http(url).get()(function(error, response, body) {
+    var img_src, img_title, selectors;
+    if (error) {
+      return response_handler("Sorry, something went wrong");
+    }
+    if (response.statusCode === 302) {
+      location = response.headers['location'];
+      return send_meme(message, location, response_handler);
+    }
+    selectors = ["a img.badge-item-img"];
+    if (process.env.HUBOT_9GAG_NO_GIFS == null) {
+      selectors.unshift("div.badge-animated-container-animated img");
+    }
+    img_src = get_meme_image(body, selectors);
+    if (img_src.substr(0, 4) !== "http") {
+      img_src = "http:" + img_src;
+    }
+    img_title = escape_html_characters(get_meme_title(body, [".badge-item-title"]));
+    return response_handler(img_title, img_src);
+  });
+};
+
+select_element = function(body, selectors) {
+  var html_handler, html_parser, i, img_container, len, selector;
+  html_handler = new HTMLParser.DefaultHandler((function() {}), {
+    ignoreWhitespace: true
+  });
+  html_parser = new HTMLParser.Parser(html_handler);
+  html_parser.parseComplete(body);
+  for (i = 0, len = selectors.length; i < len; i++) {
+    selector = selectors[i];
+    img_container = Select(html_handler.dom, selector);
+    if (img_container && img_container[0]) {
+      return img_container[0];
+    }
+  }
+};
+
+get_meme_image = function(body, selectors) {
+  return select_element(body, selectors).attribs.src;
+};
+
+get_meme_title = function(body, selectors) {
+  return select_element(body, selectors).children[0].raw;
+};
+
+escape_html_characters = function(text) {
+  var i, len, r, replacements;
+  replacements = [[/&/g, '&amp;'], [/</g, '&lt;'], [/"/g, '&quot;'], [/'/g, '&#039;']];
+  for (i = 0, len = replacements.length; i < len; i++) {
+    r = replacements[i];
+    text = text.replace(r[0], r[1]);
+  }
+  return text;
 
 }
 
